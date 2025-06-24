fe5cddbc99718b1e419b727f130908e68b0d3433
chaitey50 <chaiteyc@gmail.com>
2025-06-24 18:15:59 +0600

    search field add to browse gardening tips section

--- src/Pages/TipDetails/TipDetails.jsx
+++ src/Pages/TipDetails/TipDetails.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { BiLike } from 'react-icons/bi';
+import { FaArrowLeft } from 'react-icons/fa';
 import { Link, useParams } from 'react-router';
 
 const TipDetails = () => {
@@ -19,19 +21,46 @@ const TipDetails = () => {
       </div>
         )
     }
+    const handleLike = () => {
+        fetch(`http://localhost:3000/tips/like/${id}`,{
+            method:"PATCH",
+            headers:{
+                'content-type' : "application/json"
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.modifiedCount >0) {
+                setTip(prev => ({...prev, totalLiked: prev.totalLiked + 1}))
+            }
+        })
+    }
     return (
-         <div className="max-w-3xl mx-auto p-6 bg-white shadow rounded-lg mt-6">
+         <div className="max-w-3xl mx-auto p-6 bg-green-50 shadow-sm rounded-lg mt-6">
       <img src={tip.image} alt={tip.title} className="w-full h-60 object-cover rounded mb-4" />
-      <Link to='/browseTips'>
+      <Link to='/browseTips' className='flex gap-2 text-blue-500 items-center'>
+      <FaArrowLeft />
             <a href="">back to All tips</a>
             </Link>
       <h2 className="text-3xl font-bold text-lime-600 mb-2">{tip.title}</h2>
       <p className="mb-2"><strong>Category:</strong> {tip.category}</p>
-      <p className="mb-2"><strong>Difficulty:</strong> {tip.difficulty}</p>
+      <div className='flex gap-3 items-center'>
+        <p className="mb-2"><strong>Difficulty:</strong> {tip.difficulty}</p>
+        <p className='border p-1.5 rounded-xl'><strong>Likes:</strong> {tip.totalLiked}</p>
+      </div>
       <p className="mb-2"><strong>Plant Type:</strong> {tip.plantType}</p>
       <p className="mb-2"><strong>Author:</strong> {tip.userName}</p>
       <p className="mb-4"><strong>Description:</strong> {tip.description}</p>
-      <p><strong>Likes:</strong> {tip.totalLiked}</p>
+      <div className='flex items-center gap-2 mt-4 justify-end p-3'>
+  <button
+    onClick={handleLike}
+    className='px-4 py-1 bg-blue-400 text-white rounded shadow text-sm flex items-center gap-2 cursor-pointer'
+  >
+   <BiLike />{tip.totalLiked} Like
+  </button>
+</div>
+
     </div>
     );
 };
